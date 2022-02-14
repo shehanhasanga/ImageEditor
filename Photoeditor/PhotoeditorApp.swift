f93cd409e95b1bc3a55109f7e0df661b1165cf6f
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-02-14 17:51:45 +0530

    Initial Commit

---
+++ Photoeditor/PhotoeditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoeditorApp.swift
+//  Photoeditor
+//
+//  Created by shehan karunarathna on 2022-02-14.
+//
+
+import SwiftUI
+
+@main
+struct PhotoeditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
